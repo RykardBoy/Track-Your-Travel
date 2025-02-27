665190f160e4fc76f45d557e3690bb84baeb187b
Rykardboy <flugel.ricardo77@gmail.com>
2025-02-27 19:57:38 +0100

    modification sur la page profile

--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
+import { StyleSheet } from 'react-native';
 
-const ProfilePage = ({navigation}) => {
+const ProfilePage = () => {
     return (
-        <View>
-            <Text>Your profile</Text>
-            <Text onPress={() => navigation.navigate('Home')}>Retourner à la page d'accueil</Text>
+        <View style={styles.view1}>
+            <Text style={styles.titre1}>Hi Ricardo !</Text>
+            <View>
+                <Image source={require('../assets/boy.png')} style={styles.image1}></Image>
+            </View>
+            <View>
+                <View>
+                    <Text name="username">Nom d'utilisateur : ricardo.flugel</Text>
+                    <Text name="email">Email : rf@gmail.com</Text>
+                    <Text name="pays">Pays : Suisse</Text>
+                </View>
+            </View>
+            
         </View>
     )
 }
 
-
+const styles = StyleSheet.create({
+    view1:{
+        backgroundColor : "#afd2e0",
+        height:"100%"
+    },
+    titre1:{
+        fontSize:40,
+        textAlign:"center"
+    },
+    image1:{
+        width:200,
+        height:200,
+        alignSelf:"center",
+        marginTop:50
+    }
+})
 
 
 export default ProfilePage;
